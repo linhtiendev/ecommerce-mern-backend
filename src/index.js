41d96822a156eb595319037809464c9b55c60189
linhtiendev <lelinhtien0707@gmail.com>
2023-09-24 16:11:32 +0700

    connect to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,19 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
+
+// connect MongoDB
+mongoose
+    .connect(
+        `mongodb+srv://litimini:${process.env.MONGO_DB}@cluster0.byegi3e.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`
+    )
+    // promise
+    .then(() => {
+        console.log("Connect DB success!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(port, () => {
     console.log("Server is running in port: ", port);
