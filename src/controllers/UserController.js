45ee359cb4f44829693ad460c17ca3e0142868eb
linhtiendev <lelinhtien0707@gmail.com>
2023-09-24 22:35:45 +0700

    create API signup user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,32 @@
 const UserService = require("../services/UserService");
 
-// xử lý method tạo
+// hàm xử lý method tạo
 const createUser = async (req, res) => {
     try {
-        console.log(req.body);
-        const res = await UserService.createUser();
-        return res.status(200).json(res);
+        const { name, email, password, confirmPassword, phone } = req.body;
+        // check có phải là email hay không
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        const isCheckEmail = reg.test(email); // boolean
+        // check input
+        if (!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status: "error",
+                message: "Input is required",
+            });
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: "error",
+                message: "Input is email",
+            });
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: "error",
+                message: "Input is equal confirmPassword",
+            });
+        }
+        // nếu k rơi vào case lỗi thì sẽ gọi đến service
+        const response = await UserService.createUser(req.body);
+        return res.status(200).json(response);
     } catch (e) {
         return res.status(404).json({
             message: e,
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,37 @@
-// xử lí liên quan đến API
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+
+// Hàm xử lí liên quan đến API
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, phone } = newUser;
         try {
-            resolve({});
+            // Hàm check email đã tồn tại
+            const checkUser = await User.findOne({
+                email: email,
+            });
+            if (checkUser !== null) {
+                resolve({
+                    status: "OK",
+                    message: "Email is already",
+                });
+            }
+            // hàm hashpassword (mã hóa)
+            const hash = bcrypt.hashSync(password, 10);
+            // Hàm tạo user
+            const createUser = await User.create({
+                name,
+                email,
+                password: hash,
+                phone,
+            });
+            if (createUser) {
+                resolve({
+                    status: "OK",
+                    message: "User created!",
+                    data: createUser,
+                });
+            }
         } catch (e) {
             reject(e);
         }
